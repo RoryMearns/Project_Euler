7daed0508380b8955f2a6ebc6bb478223bf867fc
Rory Mearns <r.mearnsy@gmail.com>
2015-06-28 15:18:38 +1200

    Added another solution

--- ProjectEuler.java
+++ ProjectEuler.java
@@ -110,7 +110,7 @@ public class ProjectEuler {
 			squareOfSum = squareOfSum + (i);
 		}
 		squareOfSum *= squareOfSum;
-		System.out.println("The answer to Project 5 is: " + (squareOfSum - sumOfSquares));
+		System.out.println("The answer to Project 6 is: " + (squareOfSum - sumOfSquares));
 	}
 
 	public void	project07 () { 	// 10001st prime
@@ -125,7 +125,7 @@ public class ProjectEuler {
 				counter++;
 			}
 		}
-		System.out.println("The answer to Project 6 is: " + number);
+		System.out.println("The answer to Project 7 is: " + number);
 	}
 
 	public boolean isPrime (int x) {	// Helper method for Project 7
@@ -138,6 +138,22 @@ public class ProjectEuler {
 		return true;
 	}
 
+	public void project08 () {	// Largest product in a series
+		// Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+
+		String theNumber = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+		long maxProduct = 0;
+		long currentProduct;
+
+		for (int i=0; i<theNumber.length()-13; i++) {
+			currentProduct = 1;
+			for (int k=i; k<i+13; k++) {
+				currentProduct = currentProduct * Integer.parseInt(theNumber.substring(k, k + 1));
+				maxProduct = (currentProduct>maxProduct)? currentProduct : maxProduct;
+			}
+		}
+		System.out.println("The answer to Project 8 is: " + maxProduct);
+	}
 
 	public static void main (String[] args) {
 		ProjectEuler project = new ProjectEuler();
@@ -147,7 +163,8 @@ public class ProjectEuler {
 		// project.project04();
 		// project.project05();
 		// project.project06();
-		project.project07();
+		// project.project07();
+		project.project08();
 	}
 }
 
